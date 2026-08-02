1ee31e49d577c4c3756b8a4b9a0dacc935f27c9e
msaber1 <m.saber@elnadycompany.com>
2026-08-02 04:03:09 +0300

    projects module

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// Authenticated routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-}); 
-  
+    // Projects Module
+    Route::apiResource('projects', ProjectController::class);
+
+});
